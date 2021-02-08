cbe58907fd13c412f8a3392d625ab7b90e6d81dc
ygorsansone <ygorsansone@gmail.com>
2021-02-08 11:54:58 -0300

    aws configs

---
+++ src/views/Loginaws.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="app">
+      <div v-if="!signedIn">
+         <amplify-authenticator></amplify-authenticator>
+      </div>
+      <div v-if="signedIn">
+        <amplify-sign-out class="signout"></amplify-sign-out>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+</template>
+
+<script>
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import { Auth } from 'aws-amplify'
+export default {
+  name: 'app',
+  async beforeCreate() {
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      console.log(user)
+      this.signedIn = true
+    } catch (err) {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      if (info === 'signedIn') {
+        this.signedIn = true
+      } else {
+        this.signedIn = false
+      }
+    });
+  },
+  data () {
+    return {
+      signedIn: false
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.signout {
+  background-color: #ededed;
+  margin: 0;
+  padding: 11px 0px 1px;
+}
+</style>
