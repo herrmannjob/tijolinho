d1a28ef1dcb8c8af61031d50d28911106b6f38ce
Luiz Freitas <luiz_dev@outlook.com>
2021-02-12 14:16:19 -0300

    [UPD] - more functions on functions.js

--- src/functions/Functions.js
+++ src/functions/Functions.js
@@ -1,16 +1,72 @@
 import { Auth } from 'aws-amplify'
+import { DataStore, Predicates } from 'aws-amplify'
 
 const functions = {
-    async login (router, email, password) {
+    // LOGIN
+    async login (email, password) {
         try {
-            const user = await Auth.signIn(email, password)
-            console.log(user)
-            router.push('calendar')
+            await Auth.signIn(email, password)
+            return { status: 'ok' }
         } catch (error) {
-            alert(error)
+            return { status: 'error', error }
         }
     },
-    loginCognito (router) { router.push('aws') }
+
+    // USUÁRIO AUTENTICADO?
+    async isAuth () {
+        const response = await Auth.currentAuthenticatedUser()
+        return response
+    },
+
+    //====== TABELAS DO BANCO ======//
+
+    //=== LISTAR ===//
+    
+    //= BASEADO NUM ID ESPECÍFICO =//
+    async getById (table, id) {
+        try {
+            let data = []
+            const items = await DataStore.query(table, d => d.usuarioID("eq", id))
+            if (items.length > 0) {
+                items.map(item => {
+                    data.push(item.nome)
+                })
+                return { status: 'ok', data }
+            } else return { status: 'empty' }
+        } catch (error) {
+            return { status: "error", error }
+        }
+    },
+
+    //= TODOS DA TABELA =//
+    async getAll (table) {
+        try {
+            let data = []
+            const items = await DataStore.query(table, Predicates.ALL)
+            if (items.length > 0) {
+                items.map(item => {
+                    if (table.name === 'Tarefa') data.push({ title: item.nome_tarefa, start: item.data_inicio.substr(0, 10), end: item.data_fim.substr(0, 10) })
+                    else data.push(item.nome)
+                })
+                return { status: 'ok', data }
+            } else return { status: 'empty' }
+        } catch (error) {
+            return { status: "error", error }
+        }
+    },
+
+    //=== INSERIR ===//
+
+    async putData (table, data) {
+        try {
+            await DataStore.save(
+                new table(data)
+            )
+            return { status: 'ok' }
+        } catch (error) {
+            return { status: "error", error }
+        }
+    }
 }
 
 export default functions
