9935048352e576e29479b22f6cfcda3aedc19e47
ygorsansone <ygorsansone@gmail.com>
2021-02-18 22:34:41 -0300

    cadastrando varias tarefas

--- src/functions/tarefas.js
+++ src/functions/tarefas.js
@@ -1,5 +1,6 @@
-// import { DataStore } from 'aws-amplify'
-var _GLOBAL_ID = 1
+import { Tarefa } from '@/models'
+import Functions from '@/functions/Functions'
+// var _GLOBAL_ID = 1
 class TarefaWrapper {
 	constructor(nome){
 		this.nome = nome
@@ -15,57 +16,84 @@ class TarefaWrapper {
 	addOrigem(origem){
 		this.origem = origem
 	}
-	sendToAmplify() {
+	async sendToAmplify() {
 		if (this.origem) {
 			if (this.origem.ID == 0){
 				this.origem.sendToAmplify()
 			}
 		}
+			const response = await Functions.putData(Tarefa, {
+			// "CronogramaObra": /* Provide a CronogramaObra instance here */,
+			// "Responsavel": /* Provide a Usuario instance here */,
+			// "status": /* Provide a StatusTarefa instance here */,
+			"nome_tarefa": this.nome,
+			"data_inicio": '2021-02-01',
+			"data_fim": '2021-02-01',
+			// "TarefaOrigem": /* Provide a Tarefa instance here */
+			})
+			console.log(response);
+		
 		// send to amplify
-        console.log(this.nome);
-        this.ID = _GLOBAL_ID++;
-        console.log(this.ID);
+		// console.log(this.nome);
+		// this.ID = _GLOBAL_ID++;
+		// console.log(this.ID);
+	}
+	delete() {
+		if (this.origem){
+			this.filhos.map( key => {
+				key.addOrigem(this.origem) | this.origem.addFilho(key)
+			})
+			this.origem.filhos = this.origem.filhos.filter(key => key.nome.localeCompare(this.nome))
+		}
 	}
 }
-const _json = `{"tarefa": [{"filho1": []}, {"filho2": []}]}`;
+const _json = `{"avo": [{"tarefa": [{"filho1": []}, {"filho2": []}]}], "velho":[]}`;
 const Tarefas = {
+	addChildren (key, mapTarefas, prefix) {
+		var father = key[0]
+		var children = key[1]
+		var father_prefix = String(prefix + '.' + father)
+		mapTarefas.set(father_prefix, new TarefaWrapper(father))
+		console.log("estou procurando pelo "+prefix)
+		if (mapTarefas.get(prefix) != undefined){
+			console.log("colocando", father_prefix, "no", father_prefix)
+			mapTarefas.get(prefix)
+				.addFilho(mapTarefas.get(father_prefix))
+		}
+		mapTarefas.get(father_prefix)
+			.addOrigem(mapTarefas.get(prefix))
+		children.map(key => {
+			Tarefas.addChildren(Object.entries(key)[0]
+				, mapTarefas, father_prefix)
+		})
+	},
 
-  addChildren (key, mapTarefas, prefix) {
-	var father = key[0]
-	var children = key[1]
-	var father_prefix = String(prefix + '.' + father)
-	mapTarefas.set(father_prefix, new TarefaWrapper(father))
-	if (mapTarefas.get('.' + prefix) != undefined){
-		mapTarefas.get('.'+ prefix)
-			.addFilho(mapTarefas.get(father_prefix))
+	serializeToMap (mapJson, mapTarefas) {
+		Object.entries(mapJson).map(key => {
+			Tarefas.addChildren(key, mapTarefas, '')
+		})
+	},
+
+	serializeJson(jsonFile) {
+		var mapJson = JSON.parse(jsonFile)
+		var mapTarefas = new Map()
+		Tarefas.serializeToMap(mapJson, mapTarefas)
+		return mapTarefas
+	},
+
+	deleteFromMapTarefas(mapTarefas, key){
+		var element = mapTarefas.get(key)
+		if (element){
+			element.delete()
+			mapTarefas.delete(key)
+		}
+	},
+
+	async cadastra(){
+		var mapTarefas = Tarefas.serializeJson(_json)
+		mapTarefas.forEach(key => {
+			key.sendToAmplify()
+		});
 	}
-	mapTarefas.get(father_prefix)
-		.addOrigem(mapTarefas.get('.' + prefix))
-	children.map(key => {
-		Tarefas.addChildren(Object.entries(key)[0]
-			, mapTarefas, father)
-	})
-},
-
- serializeToMap (mapJson, mapTarefas) {
-	Object.entries(mapJson).map(key => {
-		Tarefas.addChildren(key, mapTarefas, '')
-	})
-},
-
- serializeJson(jsonFile) {
-	var mapJson = JSON.parse(jsonFile)
-	var mapTarefas = new Map()
-	Tarefas.serializeToMap(mapJson, mapTarefas)
-	return mapTarefas
-},
-
-async cadastra(){
-    console.log('oi');
-    console.log(Tarefas.serializeJson(_json));
-    Tarefas.serializeJson(_json).forEach(key => {
-        key.sendToAmplify()
-    });
-}
 };
 export default Tarefas
