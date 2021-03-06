166316e5923614374bddc55b226848782e0319d6
ygorsansone <ygorsansone@gmail.com>
2021-03-06 19:09:31 -0300

    configurações do firebase

---
+++ src/services/Firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/auth'
+import 'firebase/analytics'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNeI54GGdOpqTzGeirDSPmaZx4QkLNbfc",
+    authDomain: "tijolinho-3b66e.firebaseapp.com",
+    databaseURL: "https://tijolinho-3b66e-default-rtdb.firebaseio.com",
+    projectId: "tijolinho-3b66e",
+    storageBucket: "tijolinho-3b66e.appspot.com",
+    messagingSenderId: "562010994697",
+    appId: "1:562010994697:web:c7cd7fc0cb1313db0020d8",
+    measurementId: "G-4QT6GW12TW"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().enablePersistence()
+  export default firebase
